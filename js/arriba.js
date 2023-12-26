8db7c999baec63ed95e0135d26f0fc114654004d
“Gianna <giannagiavarini@outlook.com>
2023-12-26 02:14:13 -0300

    se modifica estilos de flecha ir arriba

--- js/arriba.js
+++ js/arriba.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
     $('.ir-arriba').click(function(){
         $('body, html').animate({
             scrollTop: '0px'
-        }, 300);
+        }, 200);
     });
 
-    $(window).scroll(function(){
-        if ( $(this).scrollTop() > 0 ){
-            $('ir-arriba').slideDown(200);
+     $(window).scroll(function(){
+          if ( $(this).scrollTop() >0 ){
+           $('ir-arriba').slideDown(200);
         }else {
-            $('ir-arriba').slideUp(200);
-        }
-    });
+             $('ir-arriba').slideUp(200);
+         }
+     });
 });
