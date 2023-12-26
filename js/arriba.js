888893e0ebe1ebadc759c7ff55945f812396a92c
“Gianna <giannagiavarini@outlook.com>
2023-12-26 02:05:42 -0300

    se agrega flecha ir arriba

---
+++ js/arriba.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.ir-arriba').click(function(){
+        $('body, html').animate({
+            scrollTop: '0px'
+        }, 300);
+    });
+
+    $(window).scroll(function(){
+        if ( $(this).scrollTop() > 0 ){
+            $('ir-arriba').slideDown(200);
+        }else {
+            $('ir-arriba').slideUp(200);
+        }
+    });
+});
